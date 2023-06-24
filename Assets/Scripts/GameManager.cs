d3d7c2d3566fc2069bfd0a26377f3c1bccf0f10e
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2023-06-24 15:41:48 -0300

    Add GameOver and Retry button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,6 +9,9 @@ public class GameManager : MonoBehaviour
     public float initialGameSpeed = 5f;
     public float gameSpeedIncrease = 0.1f;
     public float gameSpeed { get; private set; }
+
+    public TextMeshProUGUI gameOverText;
+    public Button retryButton;
 
     private Player player;
     private Spawner spawner;
@@ -44,6 +49,9 @@ public class GameManager : MonoBehaviour
         enabled = true;
         player.gameObject.SetActive(true);
         spawner.gameObject.SetActive(true);
+
+        gameOverText.gameObject.SetActive(false);
+        retryButton.gameObject.SetActive(false);
     }
 
     public void GameOver() {
@@ -52,6 +60,9 @@ public class GameManager : MonoBehaviour
 
         player.gameObject.SetActive(false);
         spawner.gameObject.SetActive(false);
+
+        gameOverText.gameObject.SetActive(true);
+        retryButton.gameObject.SetActive(true);
     }
 
     private void Update() {
