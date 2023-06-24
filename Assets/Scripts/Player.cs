a0a225ff97c670c44a32324349b49785d8018e64
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2023-06-24 14:10:11 -0300

    Add initial Player, Ground and GameManager

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController character;
+    private Vector3 direction;
+
+    public float gravity = 9.81f * 2f;
+    public float jumpForce = 8f;
+
+    private void Awake() {
+        character = GetComponent<CharacterController>();
+    }
+
+    private void OnEnable() {
+        direction = Vector3.zero;
+    }
+
+    private void Update() {
+        direction += Vector3.down * gravity * Time.deltaTime;
+
+        if (character.isGrounded) {
+            direction = Vector3.down;
+
+            if (Input.GetButton("Jump") || Input.GetKey(KeyCode.UpArrow)) {
+                direction = Vector3.up * jumpForce;
+            }
+        }
+
+        character.Move(direction * Time.deltaTime);
+    }
+}
